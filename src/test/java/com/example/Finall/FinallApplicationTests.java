a04511f81acb8d305e76bf46adb31e033179c1bd
GOOSE <aiberiskendirov@gmail.com>
2026-01-11 23:57:11 +0500

    iasibnskii

---
+++ src/test/java/com/example/Finall/FinallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Finall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
