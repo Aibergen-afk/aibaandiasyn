19af8b3236db8f84396b27027abbbc170075e055
Iassin <iassin.zinulla@narxoz.kz>
2026-01-12 18:17:46 +0500

    add model

--- src/main/java/com/example/Finall/model/Book.java
+++ src/main/java/com/example/Finall/model/Book.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Table(name = "books")
 @Getter
@@ -18,6 +21,16 @@ public class Book {
 
     private Integer year;
 
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private Author author;
 
+    @ManyToMany
+    @JoinTable(
+            name = "book_categories",
+            joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private Set<Category> categories = new HashSet<>();
 }
 
